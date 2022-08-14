b9be0daa5e2ddc6ac2e60e3acf3b4154e6f9746f
Jeffrey <jeffreyhu16@gmail.com>
2022-08-14 18:28:46 +0800

    add frontend skill description

--- src/components/SkillsPage.jsx
+++ src/components/SkillsPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import LinkParticle from './subComponents/LinkParticle';
 import SocialIcons from './subComponents/SocialIcons';
+import { BulletPoint } from './subComponents/Svgs';
 
 const Box = styled.div`
     background-color: ${props => props.theme.body};
@@ -17,13 +18,16 @@ const Main = styled.div`
     }
 `
 
-const Title = styled.div`
-
-`
-
 const Description = styled.div`
     color: ${props => props.theme.color};
+`
 
+const Bullet = styled.div`
+    ${Main}:hover &{
+        &{
+            fill: #dcdcdc
+        }
+    }
 `
 
 export default function SkillsPage(props) {
@@ -32,16 +36,39 @@ export default function SkillsPage(props) {
 
     return (
         <Box className='skills-container'>
-            <Main className='skills-content flex'>
-                <Title className='skills-content-title'>Frontend</Title>
+            <Main className='skills-content'>
+                <h3 className='skills-content-title'>Frontend</h3>
                 <Description className='skills-content-desc'>
-                    
+                    <div className="skills-point-container">
+                        <Bullet>
+                            <BulletPoint />
+                        </Bullet>
+                        <div className="skills-point">Created a professional calendar design using pure css with flexbox</div>
+                    </div>
+                    <div className="skills-point-container">
+                        <Bullet>
+                            <BulletPoint />
+                        </Bullet>
+                        <div className="skills-point">Utilized Redux to improve render performace by over 100%</div>
+                    </div>
+                    <div className="skills-point-container">
+                        <Bullet>
+                            <BulletPoint />
+                        </Bullet>
+                        <div className="skills-point">Incorporated Material UI's Data Grid Component to display/edit data</div>
+                    </div>
+                    <div className="skills-point-container">
+                        <Bullet>
+                            <BulletPoint />
+                        </Bullet>
+                        <div className="skills-point">Integrated animations using Three.js with performance in mind</div>
+                    </div>
                 </Description>
             </Main>
             <Main className='skills-content flex'>
-                <Title className='skills-content-title'>Blockchain</Title>
+                <h2 className='skills-content-title'>Blockchain</h2>
                 <Description className='skills-content-desc'>
-                    
+
                 </Description>
             </Main>
             <LinkParticle />
